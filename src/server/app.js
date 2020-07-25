777b48b59592633f53c8b091a6ebb57d668c67b8
Haixiang6123 <haixiang6123@gmail.com>
2020-07-25 15:25:19 +0800

    refactor: 去掉无用文件

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 
 const resultsRouter = require('./routes/results');
 
@@ -8,7 +7,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/results', resultsRouter);
 
