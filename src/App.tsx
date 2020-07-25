6921a16e9da09f58c7ba4078f5c2425edec828d1
Haixiang <haixiang6123@gmail.com>
2020-07-25 11:41:09 +0800

    chore: 添加注释

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ const App: FC<IProps> = () => {
       <h1 style={{textAlign: 'center'}}>很牛逼的一个计算器 v1.0</h1>
       <h2>加法</h2>
       <Adder/>
+      {/* 请在这里添加你的减法器 */}
+      <h2>减法</h2>
     </div>
   )
 }
