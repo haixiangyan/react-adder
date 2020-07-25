dd4a40ebd188441c60909015356d30563bf6d123
Haixiang6123 <haixiang6123@gmail.com>
2020-07-25 15:24:33 +0800

    feat: 添加 Express 服务器

---
+++ src/server/app.js
@@ -0,0 +1,27 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+
+const resultsRouter = require('./routes/results');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/results', resultsRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
