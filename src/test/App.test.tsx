af2f0a30c9a10ac2a138200c16764906b534753f
Haixiang6123 <haixiang6123@gmail.com>
2020-07-25 14:20:23 +0800

    test: 添加更多的测试

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,9 +2,11 @@ import {add} from "../components/Adder"
 
 describe('add 函数', () => {
   it('正确计算', () => {
-    const result = add(1, 1)
+    for (let i = 0; i < 10; i++) {
+      const result = add(i, i)
 
-    expect(result).toEqual(2)
+      expect(result).toEqual(i * 2)
+    }
   })
 
   it('处理异常', () => {
