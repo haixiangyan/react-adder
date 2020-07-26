d209815a4247a1853d0b8f4bb331edfc24d29f18
Haixiang <haixiang6123@gmail.com>
2020-07-26 18:26:30 +0800

    feat: 将 add 函数改成 loopAdd

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,14 @@
-import {add} from '../components/Adder'
+import {loopAdd} from '../components/Adder'
 
 describe('add 函数', () => {
   it('正确计算', () => {
-    const result = add(1)
+    const result = loopAdd(1)
 
     expect(result).toEqual(2)
+  })
+  it('大于等于10的情况', () => {
+    const result = loopAdd(9)
+
+    expect(result).toEqual(0)
   })
 })
