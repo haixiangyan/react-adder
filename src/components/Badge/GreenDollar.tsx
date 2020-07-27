8b44909ecad03d99e92a0f0635afafb1db337cef
Haixiang6123 <haixiang6123@gmail.com>
2020-07-27 21:00:51 +0800

    feat: 添加 Badge 组件

---
+++ src/components/Badge/GreenDollar.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import {FC} from "react"
+
+interface IProps {
+}
+
+const GreenDollar: FC<IProps> = () => {
+  return <span>$</span>
+}
+
+export default GreenDollar
---
+++ src/components/Badge/RedCross.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import {FC} from "react"
+
+interface IProps {
+}
+
+const RedCross: FC<IProps> = () => {
+  return <span className="red-cross">x</span>
+}
+
+export default RedCross
